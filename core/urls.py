4432d85eee22156ea2faf408b3aa91f0f65ba615
nimadorostkar <nimadorostkar97@gmail.com>
2021-07-21 14:20:27 +0430

    core urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include  # add this
-
+from django.urls import path, include  
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),          # Django admin route
-    path("", include("authentication.urls")), # Auth routes - login / register
-    path("", include("app.urls"))             # UI Kits Html files
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path("", include("app.urls"))
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
