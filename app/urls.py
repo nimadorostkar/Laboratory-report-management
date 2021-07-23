10d051b5ee1ebc5ab05dac0583ba151c7077fe33
nimadorostkar <nimadorostkar97@gmail.com>
2021-07-23 21:31:39 +0430

    kkk

--- app/urls.py
+++ app/urls.py
@@ -6,15 +6,12 @@ app_name='app'
 
 
 
-
-
 urlpatterns = [
 
     # The home page
     path('', views.index, name='index'),
     path('profile', views.profile, name='profile'),
     path('about', views.about, name='about'),
-
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
